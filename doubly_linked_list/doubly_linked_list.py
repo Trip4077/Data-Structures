28330b3e2863998c124b029614ed30645d5f2fc7
Trip4077 <bj2336@email.vccs.edu>
2019-12-30 04:33:27 -0500

    Doubly Linked List Complete

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -8,6 +8,9 @@ class ListNode:
         self.prev = prev
         self.next = next
 
+    def __str__(self):
+        return "Value of " + str(self.value)
+
     """Wrap the given value in a ListNode and insert it
     after this node. Note that this node could already
     have a next node it is point to."""
@@ -48,6 +51,14 @@ class DoublyLinkedList:
     def __len__(self):
         return self.length
 
+    def print(self):
+        curr_node = self.head
+        print(curr_node)
+
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+            print(curr_node)
+
     """Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
@@ -69,12 +80,9 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        new_head = self.head.next
-        self.length -= 1
-
-        new_head.prev = None
-        self.head.next = None
-        self.head = new_head
+        value = self.head.value
+        self.delete(self.head)
+        return value
         
 
     """Wraps the given value in a ListNode and inserts it 
@@ -86,7 +94,7 @@ class DoublyLinkedList:
 
         if not self.head and not self.tail:
             self.head = new_node
-            self.tail - new_node
+            self.tail = new_node
         else:
             new_node.prev = self.tail
             self.tail.next = new_node
@@ -96,17 +104,29 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head: return
+
+        value = node.value
+
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail: return
+
+        value = node.value
+
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
@@ -116,20 +136,59 @@ class DoublyLinkedList:
         if self.head == self.tail:
             self.head = None
             self.tail = None
+            self.length -= 1
 
         elif self.head == node:
             self.head = node.next
             node.delete()
+            self.length -= 1
 
         elif self.tail == node:
             self.tail = node.prev
             node.delete()
-            
+            self.length -= 1
+
         else:
             node.delete()
+            self.length -= 1
 
-        self.length -= 1
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        largest = self.head.value
+        curr_node = self.head
+ 
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+            value = curr_node.value
+
+            if value > largest:
+                largest = curr_node.value
+            
+        return largest
+
+
+
+
+first_node = ListNode(100)
+
+linked_list = DoublyLinkedList(first_node)
+
+linked_list.add_to_head( 1 )
+linked_list.add_to_head( 2 )
+linked_list.add_to_head( 3 )
+
+linked_list.add_to_tail( 400 )
+linked_list.add_to_tail( 501 )
+
+#linked_list.delete(first_node)
+
+linked_list.remove_from_head()
+linked_list.remove_from_tail()
+
+# linked_list.add_to_head(first_node)
+# linked_list.add_to_tail(first_node)
+
+linked_list.print()
+print("List Length: " + str( len( linked_list ) ) )
+print("Largest Value: " + str( linked_list.get_max() ) )
